9d066a2831e29efc2567c77320672c038d47459a
KimYeonBong <yb.kim@buildit.kr>
2020-03-03 11:26:31 +0900

    SDK v1.1.5 업데이트 - dist 파일명 변경으로 인한 index.js 수정 반영

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-//만약 Webpack을 사용하지 않을 경우, 아래 코드 라인을 주석 처리하고
+// 만약 Webpack을 사용하지 않을 경우, 아래 코드 라인을 주석 처리하고
 // If you are not using Webpack, comment out the line of code below.
 
-//타겟 플랫폼에 해당하는 모듈을 참조하는 라인의 주석을 해제하세요.
+// 타겟 플랫폼에 해당하는 모듈을 참조하는 라인의 주석을 해제하세요.
 // And then, Uncomment specific line of code about reference to the module that corresponds to the target platform.
 
 // for MacOS (No Webpack Configuration)
-// var BuildThingBLE = require('./dist/buildthing.ble.electron.darwin.js')
-// for Windows (No Webpack Configuration)
-//var BuildThingBLE = require('./dist/buildthing.ble.electron.win32.js')
+// var BuildThingBLE = require('./dist/buildthing.ble.darwin.js')
+// for Windows - Node.js (No Webpack Configuration)
+// var BuildThingBLE = require('./dist/buildthing.ble.win32.node.js')
+// for Windows - Electron (No Webpack Configuration)
+// var BuildThingBLE = require('./dist/buildthing.ble.win32.electron.js')
+// for Linux - Node.js (No webpack Configuration)
+// var BuildThingBLE = require('./dist/buildthing.ble.linux.node.js')
 // for Mobile (iOS, Android) (No Webpack Configuration)
-//var BuildThingBLE =  require('./dist/buildthing.ble.cordova.js')
+// var BuildThingBLE =  require('./dist/buildthing.ble.cordova.js')
 // for Webpack alias Configuration
 var BuildThingBLE = require('buildthing-beacon-sdk')
 module.exports = BuildThingBLE
